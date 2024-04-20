ade6682afbfdd626a1ec2f07b70be7dcff3b08e1
SamAtilano <166079156+SamAtilano@users.noreply.github.com>
2024-04-19 18:40:20 -0700

    worked on js file, minor edits to css file, and added the psuedocode to lab4 index.html file

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - An implementation of the game Rock-paper-scissors
+// Author: Sam Atilano <batilano@ucsc.edu>
+// Date: April 2024
 
 // Constants
 
@@ -19,3 +19,19 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// Loop until the game is over
+    // Get the choice of Player 1 and Player 2
+    // Print Player 1's choice and Print Player 2's choice
+    // Determine which Player won
+        // If Player 1 chooses Rock and Player 2 chooses Scissors, Player 1 wins
+        // If Player 1 chooses Scissors and Player 2 chooses Paper, Player 1 wins
+        // If Player 1 chooses Paper and Player 2 chooses Rock, Player 1 wins
+        // If Player 2 chooses Rock and Player 1 chooses Scissors, Player 2 wins
+        // If Player 2 chooses Scissors and Player 1 chooses Paper, Player 2 wins
+        // If Player 2 chooses Paper and Player 1 chooses Rock, Player 2 wins
+        // If both players choose the same, it's a tie
+    // Print the result of the round
+    // Ask if the players want to play another round
+// Print the final result of the game
