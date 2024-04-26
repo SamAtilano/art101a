46538e73c004540b58345df2872c40c6e86bafe0
SamAtilano <166079156+SamAtilano@users.noreply.github.com>
2024-04-25 20:31:44 -0700

    css edits to lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -3,7 +3,6 @@
 // Date: 25 April
 
 // Define Variables
-
 make = "Honda";
 model = "Civic";
 color = "gray";
@@ -15,10 +14,10 @@ age = 2024 - year;
 
 
 // output
-document.writeIn("Make:" + make + "<br>");
-document.writeIn("Model:" + model + "<br>");
-document.writeIn("Color:" + color + "<br>");
-document.writeIn("Year:" + year + "<br>");
-document.writeIn("Age:" + age + "years<br>");
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "years<br>");
 
 
