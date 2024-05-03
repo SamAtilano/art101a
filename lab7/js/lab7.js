be97a11d6ca1f994a1b1a4bab3d653928697a487
SamAtilano <166079156+SamAtilano@users.noreply.github.com>
2024-05-02 20:49:36 -0700

    edits made to lab 7 (js file, css, and html file)

---
+++ lab7/js/lab7.js
@@ -0,0 +1,24 @@
+// index.js - Lab 7 - Functions
+// Author: Sam Atilano <batilano@ucsc.edu>
+// Date: 3 May
+
+// sortUserName - a function that takes user input and sorts the letters
+// of the name
+function sortUserName () {
+    let userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    //split string to array
+    let nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort the array
+    let nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back to a string
+    let nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    return nameSorted; 
+}
+
+//output
+document.writtenln("Oh hey, I've fixed your name:",
+    sortUserName(), "</br>");
