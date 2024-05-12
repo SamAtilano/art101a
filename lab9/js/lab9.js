7b941c2206471c7f08bd602a0b2a955eaca0dc36
SamAtilano <166079156+SamAtilano@users.noreply.github.com>
2024-05-11 22:41:58 -0700

    edits made to css and js files

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -5,13 +5,23 @@
 //add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Click Here!</button>");
 //add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-challenge").click(function(){
     //now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
 
 //add button to problems section
 $("#problems").append("<button id='button-problems'>Click Here 2!</button>");
+//add a click listener to the problems button
+$("#button-problems").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special2");
+});
 
 //add button to results section
 $("#results").append("<button id='button-results'>Click here 3!</button>");
+//add a click listener to the results button
+$("#button-results").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special3");
+});
