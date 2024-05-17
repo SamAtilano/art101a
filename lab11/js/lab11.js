ba61e55f2f1f140bb56aa267a1ec63e8dbcb9836
SamAtilano <166079156+SamAtilano@users.noreply.github.com>
2024-05-17 13:55:10 -0700

    anagram added

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -2,18 +2,34 @@
 // Author: Sam Atilano <batilano@ucsc.edu>
 // Date: 17 May
 
-//sorts the characters of a string in alphabetical order
+// Sorts the characters of a string in alphabetical order
 function sortString(inputString) {
-    //we have to convert our string to an array and back again to sort it
     return inputString.split('').sort().join('');
 }
 
-//click listener for button
-$("#submit").click(function(){
-//get value of input field 
-const userName= $("#user-name").val();
-//sort it
-userNameSorted = sortString(userName);
-//append a new div to out output div
-$("#output").html('<div class= "text"><p>' + userNameSorted + '</p></div>');
+// Checks if two strings are anagrams
+function areAnagrams(str1, str2) {
+    // If lengths are different, they cannot be anagrams
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    // Sort both strings and compare
+    return sortString(str1) === sortString(str2);
+}
+
+// Click listener for button
+$("#submit").click(function() {
+    // Get value of input fields
+    const userName = $("#user-name").val();
+    const anagramName = $("#anagram-name").val();
+    
+    // Check if they are anagrams
+    const result = areAnagrams(userName, anagramName);
+    
+    // Display the result
+    let resultMessage = userName + ' and ' + anagramName + ' are ';
+    resultMessage += result ? 'anagrams!' : 'not anagrams.';
+    
+    // Append the result to the output div
+    $("#output").html('<div class="text"><p>' + resultMessage + '</p></div>');
 });
